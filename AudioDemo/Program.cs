a9a2266fa404f7d7326fdf1989dcf1cc508834d4
Opilane TTHK <opilane@tthk.ee>
2021-11-10 13:21:28 +0200

    first commit

---
+++ AudioDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Console;
+using System.Media;
+
+namespace AudioDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                //If(OperatingSystem.IsWindows()) ei töödanud
+            
+                SoundPlayer spookyplayer = new SoundPlayer(@"C:\Users\opilane\source\repos\TITpe21\What_in_the_F_ck_was_that_-Gaming_Sound_Effect (HD).wav");
+                spookyplayer.Load();
+
+                spookyplayer.PlayLooping();
+            
+            ForegroundColor = ConsoleColor.DarkBlue;
+            WriteLine(@"
+                                              ,           ^'^  _
+                                              )               (_) ^'^
+         _/\_                    .---------. ((        ^'^
+         (('>                    )`'`'`'`'`( ||                 ^'^
+    _    /^|                    /`'`'`'`'`'`\||           ^'^
+    =>--/__|m---               /`'`'`'`'`'`'`\|
+         ^^           ,,,,,,, /`'`'`'`'`'`'`'`\      ,
+                     .-------.`|`````````````|`  .   )
+                    / .^. .^. \|  ,^^, ,^^,  |  / \ ((
+                   /  |_| |_|  \  |__| |__|  | /,-,\||
+        _         /_____________\ |')| |  |  |/ |_| \|
+       (')         |  __   __  |  '==' '=='  /_______\     _
+      (' ')        | /  \ /  \ |   _______   |,^, ,^,|    (')
+       \  \        | |--| |--| |  ((--.--))  ||_| |_||   (' ')
+     _  ^^^ _      | |__| |('| |  ||  |  ||  |,-, ,-,|   /  /
+   ,' ',  ,' ',    |           |  ||  |  ||  ||_| |_||   ^^^
+.,,|RIP|,.|RIP|,.,,'==========='==''=='==''=='=======',,....,,,,.,ldb
+== Credits ==
+Music: 'Fun House' by Coyote Hearing from youtube.com/audiolibrary
+Art: Haunted house by ldb from https://www.asciiart.eu/holiday-and-events/halloween
+");
+            ForegroundColor = ConsoleColor.Cyan;
+            WriteLine("\n(Press any key to exit)");
+            ReadKey();
+        }
+    }
+}
---
+++ ConsoleUI3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleUI3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootpath = @"C:\Users\opilane\Samples\truhvel";
+            //string[] dirs = Directory.GetDirectories(rootpath,"*", SearchOption.AllDirectories);
+
+            //foreach (string dir in dirs)
+            //{
+            //    Console.WriteLine(dir);
+            //}
+
+            //var files = Directory.GetFiles(rootpath, ".", SearchOption.TopDirectoryOnly);
+
+            //foreach (string file in files)
+            //{
+            //    //Console.WriteLine(file);
+            //    //Console.WriteLine(Path.GetFileName(file));
+            //    //Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+            //    //Console.WriteLine(Path.GetDirectoryName(file));
+            //    var info = new FileInfo(file);
+
+            //    Console.WriteLine($"{Path.GetFileName(file)}: {info.Length} bytes");
+            //}
+            //string newpath = @"C:\Users\opilane\Samples\truhvel\juss";
+            //Directory.CreateDirectory(newpath);
+            //bool  Directoryexists = Directory.Exists(newpath);
+
+            //if (Directoryexists)
+            //{
+            //    Console.WriteLine("The directory exists");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The directory doesnt exist");
+            //    Directory.CreateDirectory(newpath);
+            //}
+
+            //string[] files = Directory.GetFiles(rootpath);
+            //string destinationfolder = @"C:\Users\opilane\Samples\truhvel\SubfolderA";
+            //foreach (string file in files)
+            //{
+            //    File.Copy(file, $"{destinationfolder}{Path.GetFileName(file)}", true);            
+            //}
+
+            //for (int i = 0; i < files.Length; i++)
+            //{
+            //    File.Copy(files[i], $"{destinationfolder}{ i }.txt", false);
+            //}
+
+            //foreach (string file in files)
+            //{
+            //    File.Move(file, $"{destinationfolder}{Path.GetFileName(file)}");
+            //}
+            var files = Directory.GetFiles(rootpath, "*folder*.*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+                //Console.WriteLine(Path.GetFileName(file));
+                //Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+                //Console.WriteLine(Path.GetDirectoryName(file));
+                //var info = new FileInfo(file);
+
+                //Console.WriteLine($"{Path.GetFileName(file) }: {info.Length} bytes");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
